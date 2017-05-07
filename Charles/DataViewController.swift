5dc47fd4d4c54a3d68ba5d2007f965379494a0cf
Jacob Foster Davis <jfosterdavis@users.noreply.github.com>
2017-05-07 17:19:38 -0400

    Initial Commit

---
+++ Charles/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Charles
+//
+//  Created by Jacob Foster Davis on 5/7/17.
+//  Copyright © 2017 Zero Mu, LLC. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
