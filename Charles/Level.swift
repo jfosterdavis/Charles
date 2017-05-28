f3817f4485a3aebf13e33575fbf77980022c6348
Jacob Foster Davis <jfosterdavis@users.noreply.github.com>
2017-05-27 21:49:38 -0400

    significantly increased quality of Levels

--- Charles/Level.swift
+++ Charles/Level.swift
@@ -16,6 +16,7 @@ import Foundation
  - levelDescription: a string describing the level
  - xPRequired: amount of XP required to complete the level
  - successThreshold: Float between 0 and 1 denoting percent of a perfect score that must be achieved to earn XP in this level
+ - punishThreshold: float between 0 and 1 denoting the percent of a perfect score below or at which the user is penalized XP
  - canBeLost: Bool if the level can be lost if the user looses too much XP from higher levels
  */
 class Level: NSObject {
@@ -24,16 +25,18 @@ class Level: NSObject {
     var levelDescription: String!
     var xPRequired: Int! //an array of phrase objects
     var successThreshold: Float!
+    var punishThreshold: Float!
     var canBeLost: Bool! //radius of the corners on top
     
     // MARK: Initializers
-    init(level: Int, levelDescription: String, xPRequired: Int, successThreshold: Float, canBeLost: Bool) {
+    init(level: Int, levelDescription: String, xPRequired: Int, successThreshold: Float, punishThreshold: Float, canBeLost: Bool) {
         super.init()
         
         self.level = level
         self.levelDescription = levelDescription
         self.xPRequired = xPRequired
         self.successThreshold = successThreshold
+        self.punishThreshold = punishThreshold
         self.canBeLost = canBeLost
     }
 }
