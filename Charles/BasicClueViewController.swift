b307989de61a95471b616216b27dc87827f53c91
Jacob Foster Davis <jfosterdavis@users.noreply.github.com>
2017-06-16 04:04:25 -0400

    basic clues with images implemented

--- Charles/BasicClueViewController.swift
+++ Charles/BasicClueViewController.swift
@@ -37,21 +37,47 @@ class BasicClueViewController: UIViewController {
     }
     
     func loadClue(clue: Clue) {
+        self.clueTitle.text = clue.clueTitle
+        
         self.part1.text = clue.part1
+        
+        
         if let part1Image = clue.part1Image {
             self.part1ImageView.image = part1Image
+            
+        } else {
+            stackView.removeArrangedSubview(self.part1ImageView)
+            self.part1ImageView.removeFromSuperview()
         }
+        
         if let part2 = clue.part2 {
             self.part2.text = part2
+            
+        } else {
+            stackView.removeArrangedSubview(self.part2)
+            self.part2.removeFromSuperview()
         }
         if let part2Image = clue.part2Image {
             self.part2ImageView.image = part2Image
+            
+        } else {
+            stackView.removeArrangedSubview(self.part2ImageView)
+            self.part2ImageView.removeFromSuperview()
         }
+        
         if let part3 = clue.part3 {
             self.part3.text = part3
+            
+        } else {
+            stackView.removeArrangedSubview(self.part3)
+            self.part3.removeFromSuperview()
         }
         if let part3Image = clue.part3Image {
             self.part3ImageView.image = part3Image
+            
+        } else {
+            stackView.removeArrangedSubview(self.part3ImageView)
+            self.part3ImageView.removeFromSuperview()
         }
     }
     
--- Charles/Clues.swift
+++ Charles/Clues.swift
@@ -17,18 +17,54 @@ import UIKit
 struct Clues {
     
     ///the lineup is an array of dictionaries, where the key is the level at which the user should be presented with the associated clue.
-    static let Lineup: [Int : Clue] = [16 : Clue1
+    static let Lineup: [Int : Clue] = [2 : ClueNatureOfLight,
+                                       4 : ClueNatureOfColors,
+                                       6 : ClueNatureOfMan,
+                                       10: ClueNatureOfSoul,
+                                       16: ClueNatureOfMathematics
     
     
     ]
     
     
-    static let Clue1 = Clue(clueTitle: "The Nature of Light",
-                            part1: "The absence of light is darkness",
-                            part1Image: #imageLiteral(resourceName: "coin_100000Icon"),
-                            part2: "All light is a composite of three primary colors: Red, Green, and Blue.",
-                            part2Image: #imageLiteral(resourceName: "coin_1MIcon"),
-                            part3: "All colors can be descriged by the amount of each of the primary colors they contain.",
-                            part3Image: #imageLiteral(resourceName: "coin_1500Icon"))
+    static let ClueNatureOfLight = Clue(clueTitle: "The Nature of Light",
+                            part1: "The absence of light is darkness.",
+                            part1Image: #imageLiteral(resourceName: "WhiteOppositeBlack"),
+                            part2: "The opposite of Black is White.",
+                            part2Image: #imageLiteral(resourceName: "BlackOppositeWhite"),
+                            part3: "Do not confuse light with pigment, where opposite rules apply.",
+                            part3Image: nil)
+    
+    static let ClueNatureOfColors = Clue(clueTitle: "The Nature of Color",
+                            part1: "All light is a composite of three primary colors: Red, Green, and Blue.",
+                            part1Image: nil,
+                            part2: "The absence of all Red, Green, and Blue is black.",
+                            part2Image: nil,
+                            part3: "The presence of the most Red, most Green, and most Blue is White.",
+                            part3Image: nil)
+    
+    static let ClueNatureOfMan = Clue(clueTitle: "The Nature of Man",
+                            part1: "Above is what we aspire to be.",
+                            part1Image: #imageLiteral(resourceName: "ObjectiveCurve"),
+                            part2: "Below is what we are.",
+                            part2Image: #imageLiteral(resourceName: "StatusCurve"),
+                            part3: "Between is what we must achieve or reject.",
+                            part3Image: #imageLiteral(resourceName: "DifferenceCurve"))
+    
+    static let ClueNatureOfSoul = Clue(clueTitle: "The Nature of Soul",
+                            part1: "When below, we absorb our actions.",
+                            part1Image: #imageLiteral(resourceName: "StatusCurve"),
+                            part2: "When above, we shed our actions.",
+                            part2Image: #imageLiteral(resourceName: "WhenAbove"),
+                            part3: "When we achieve our aspiration, we lack nothing.  Nothing is zero.  Zero is black.",
+                            part3Image: #imageLiteral(resourceName: "WeLackNothing"))
+    
+    static let ClueNatureOfMathematics = Clue(clueTitle: "The Nature of Mathematics",
+                                       part1: "White is maximum, the ultimate, which cannot be exceeded.  White, plus any color, will always be White.",
+                                       part1Image: nil,
+                                       part2: "Black is zero, nothingness, which cannot be reduced.  Any color, plus or minus Black, will remain unchanged.",
+                                       part2Image: nil,
+                                       part3: "When White is removed from any color, the result will always be Black.",
+                                       part3Image: nil)
     
 }
