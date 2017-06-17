fc137fdc23a0e21a5562cb4eac37c4a75f654ee5
Jacob Foster Davis <jfosterdavis@users.noreply.github.com>
2017-06-16 23:46:04 -0400

    improved clues with more pictures, more consistent language, and some lessons earlier.

--- Charles/Clues.swift
+++ Charles/Clues.swift
@@ -18,10 +18,11 @@ struct Clues {
     
     ///the lineup is an array of dictionaries, where the key is the level at which the user should be presented with the associated clue.
     static let Lineup: [Int : Clue] = [2 : ClueNatureOfLight,
-                                       4 : ClueNatureOfColors,
-                                       6 : ClueNatureOfMan,
-                                       10: ClueNatureOfSoul,
-                                       16: ClueNatureOfMathematics
+                                       3 : ClueNatureOfColors,
+                                       5 : ClueNatureOfMan,
+                                       8: ClueNatureOfYoungSoul,
+                                       13: ClueNatureOfAdolescentSoul,
+                                       21: ClueNatureOfMathematics
     
     
     ]
@@ -38,9 +39,9 @@ struct Clues {
     static let ClueNatureOfColors = Clue(clueTitle: "The Nature of Color",
                             part1: "All light is a composite of three primary colors: Red, Green, and Blue.",
                             part1Image: nil,
-                            part2: "The absence of all Red, Green, and Blue is black.",
+                            part2: "The absence of all primary color - Red, Green, and Blue - is: zero.  Zero is Black.",
                             part2Image: nil,
-                            part3: "The presence of the most Red, most Green, and most Blue is White.",
+                            part3: "The presence of the maximum amount of Red, Green, and Blue is: maximum.  Maximum is White.",
                             part3Image: nil)
     
     static let ClueNatureOfMan = Clue(clueTitle: "The Nature of Man",
@@ -51,20 +52,29 @@ struct Clues {
                             part3: "Between is what we must achieve or reject.",
                             part3Image: #imageLiteral(resourceName: "DifferenceCurve"))
     
-    static let ClueNatureOfSoul = Clue(clueTitle: "The Nature of Soul",
-                            part1: "When below, we absorb our actions.",
-                            part1Image: #imageLiteral(resourceName: "StatusCurve"),
-                            part2: "When above, we shed our actions.",
-                            part2Image: #imageLiteral(resourceName: "WhenAbove"),
-                            part3: "When we achieve our aspiration, we lack nothing.  Nothing is zero.  Zero is black.",
-                            part3Image: #imageLiteral(resourceName: "WeLackNothing"))
+    static let ClueNatureOfYoungSoul = Clue(clueTitle: "The Nature of Young Soul",
+                            part1: "The soul is a fickle thing.  It's perspective determines the impact of our actions.",
+                            part1Image: nil,
+                            part2: "When below, we absorb.  We catch that which acts upon us.",
+                            part2Image: #imageLiteral(resourceName: "StatusCurve"),
+                            part3: "When above, we shed.  We discard that which acts upon us.",
+                            part3Image: #imageLiteral(resourceName: "WhenAbove"))
+                            
+    
+    static let ClueNatureOfAdolescentSoul = Clue(clueTitle: "The Nature of Adolescent Soul",
+                           part1: "Every soul must take a journey.  To succeed, it must know the aspiration and the obstacle.",
+                           part1Image: #imageLiteral(resourceName: "BeginningOfJourney"),
+                           part2: "When we achieve that which we aspire to achieve, we lack nothing.  Nothing is zero.  Zero is Black.  The circle is complete.",
+                           part2Image: #imageLiteral(resourceName: "WeLackNothing"),
+                            part3: "When we completely fail to achieve what we must achieve, we lack everything.  Everything is maximum.  Maximum is White.  The circle is broken; the White light burns through our core as a testiment of the everything we lack.",
+                            part3Image: #imageLiteral(resourceName: "OppositesIcon"))
     
     static let ClueNatureOfMathematics = Clue(clueTitle: "The Nature of Mathematics",
-                                       part1: "White is maximum, the ultimate, which cannot be exceeded.  White, plus any color, will always be White.",
-                                       part1Image: nil,
-                                       part2: "Black is zero, nothingness, which cannot be reduced.  Any color, plus or minus Black, will remain unchanged.",
-                                       part2Image: nil,
-                                       part3: "When White is removed from any color, the result will always be Black.",
-                                       part3Image: nil)
+                           part1: "White is maximum, the ultimate, which cannot be exceeded.  Any color which absorbs White will always become White.  Any color which sheds White will always become Black.",
+                           part1Image: nil,
+                           part2: "Black is zero, nothingness, which cannot be reduced.  Any color shedding or absorbing Black will remain unchanged - for something cannot be impacted by nothing.",
+                           part2Image: nil,
+                           part3: "A color absorbing any primary will always become (or remain) a color with the maximum of this primary.  A color shedding any primary will always become void of all traces of this primary.",
+                           part3Image: nil)
     
 }
